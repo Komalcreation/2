ad028d957e928a5411a18dd98ccb49caffe2da47
Komalcreation <komalcreationnabha@gmail.com>
2026-06-24 09:22:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@
 
 const SUPABASE_CONFIG = {
     // Example: 'https://xyzcompany.supabase.co'
-    URL: 'https://tawpkhxjnkjedkudapjw.supabase.co', 
+    URL: 'url.supabase.co', 
     
     // Example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6...'
-    ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhd3BraHhqbmtqZWRrdWRhcGp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxOTE2MDEsImV4cCI6MjA5Nzc2NzYwMX0.hLhPcBMr13IhWd-ZkhWsiW8sbS40t2aVnRJXXhLgDKY'
+    ANON_KEY: 'key'
 };
